49c5a4eba6034b1b4f0b9d892c2ec8545f9ed188
AngelicCode <angelmd81666@gmail.com>
2025-04-20 20:43:28 +0530

    list and unlist managed

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -12,12 +12,20 @@ router.post("/login",adminController.login);
 router.get("/",adminAuth,adminController.loadDashboard);
 router.get("/logout",adminController.logout);
 
+//Customer management
 router.get("/users",adminAuth,customerController.customerInfo);
 
 router.get("/blockCustomer",adminAuth,customerController.customerBlocked);
 router.get("/unblockCustomer",adminAuth,customerController.customerunBlocked);
 
+//Category management
 router.get("/category",adminAuth,categoryController.categoryInfo);
 router.post("/addCategory",adminAuth,categoryController.addCategory);
+/*router.post("/addCategoryOffer",adminAuth,categoryController.addCategoryOffer);
+router.post("/removeCategoryOffer",adminAuth,categoryController.removeCategoryOffer);*/
+router.get("/listCategory",adminAuth,categoryController.getListCategory);
+router.get("/unlistCategory",adminAuth,categoryController.getUnlistCategory);
+
+
 
 module.exports = router;
