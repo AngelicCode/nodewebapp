528fdf5d48f01de1d1cee1219707e36d70aed073
AngelicCode <angelmd81666@gmail.com>
2025-07-06 17:17:05 +0530

    product list pagination,out of stock,admin product table order are fixed

--- controllers/admin/customerController.js
+++ controllers/admin/customerController.js
@@ -1,44 +1,38 @@
   const User = require("../../models/userSchema");
 
   const customerInfo = async (req, res) => {
-    try {
-      let search = req.query.search || "";
-      let page = parseInt(req.query.page) || 1;
-      const limit = 3;
+  try {
+    let search = req.query.search || "";
+    let page = parseInt(req.query.page) || 1;
+    const limit = 3;
 
-      // Fetching customer data with search and pagination
-      const userData = await User.find({
-        isAdmin: false,
-        $or: [
-          { name: { $regex: ".*" + search + ".*", $options: "i" } },
-          { email: { $regex: ".*" + search + ".*", $options: "i" } },
-        ],
-      })
-        .limit(limit)
-        .skip((page - 1) * limit)
-        .exec();
+    const query = {
+      isAdmin: false,
+      $or: [
+        { name: { $regex: ".*" + search + ".*", $options: "i" } },
+        { email: { $regex: ".*" + search + ".*", $options: "i" } },
+      ],
+    };
 
-      // Counting total documents for pagination
-      const count = await User.countDocuments({
-        isAdmin: false,
-        $or: [
-          { name: { $regex: ".*" + search + ".*", $options: "i" } },
-          { email: { $regex: ".*" + search + ".*", $options: "i" } },
-        ],
-      });
+    const userData = await User.find(query)
+      .sort({ createdOn: -1 }) 
+      .limit(limit)
+      .skip((page - 1) * limit)
+      .exec();
 
-      res.render("customers", {
-        //customers: userData,
-        data: userData,
-        currentPage: page,
-        totalPages: Math.ceil(count / limit),
-        search,
-      });
-    } catch (error) {
-      console.error("Error loading customers:", error.message);
-      res.status(500).send("Internal Server Error");
-    }
-  };
+    const count = await User.countDocuments(query);
+
+    res.render("customers", {
+      data: userData,
+      currentPage: page,
+      totalPages: Math.ceil(count / limit),
+      search,
+    });
+  } catch (error) {
+    console.error("Error loading customers:", error.message);
+    res.status(500).send("Internal Server Error");
+  }
+};
 
   const customerBlocked = async (req,res)=>{
     try{
